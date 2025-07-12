2af2a606f3799beba1e69dd237222e18d64140f8
Kuba Suder <jakub.suder@gmail.com>
2025-07-12 22:31:20 +0200

    gpt's suggestions

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 dir=$(dirname "$0")
-cd "$dir"
+cd "$dir" || exit 1
 
 path=$(pwd)
 
-for filename in `ls *.txt`; do
-  name=`basename $filename .txt`
-  echo "Linking $path/$filename to $HOME/.$name..."
+for filename in *.txt; do
+  name=$(basename "$filename" .txt)
+  echo "Linking $path/$filename → $HOME/.$name..."
   ln -s $@ "$path/$filename" "$HOME/.$name"
 done
 
