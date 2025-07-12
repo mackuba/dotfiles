d2cdb9d8adde5e65ea678d573aae4f8197abfcbb
Kuba Suder <kuba@Ciri.local>
2025-07-12 22:21:45 +0200

    tweaked install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for filename in `ls $HOME/Projects/dotfiles`; do
+for filename in `ls $HOME/Projects/dotfiles/*.txt`; do
   name=`basename $filename .txt`
   echo "Linking $HOME/Projects/dotfiles/$filename to $HOME/.$name..."
-  ln -fs $HOME/Projects/dotfiles/$filename $HOME/.$name
+  ln -s $@ $HOME/Projects/dotfiles/$filename $HOME/.$name
 done
 
 echo "Done."
